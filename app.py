80669d068335273cbde6817088ee846207e26793
daquanzhou <daquanzhou@bytedance.com>
2024-02-22 17:47:01 +0800

    demo init

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import random
 import sys
 from typing import Sequence, Mapping, Any, Union
 import torch
+import gradio as gr
 
 
 def get_value_at_index(obj: Union[Sequence, Mapping], index: int) -> Any:
@@ -400,10 +401,125 @@ class MagicMeController:
                 unique_id=5059112282155244564,
             )
 
+import_custom_nodes()
+c = MagicMeController()
 
-def main():
-    import_custom_nodes()
+
+
+css = """
+.toolbutton {
+    margin-buttom: 0em 0em 0em 0em;
+    max-width: 2.5em;
+    min-width: 2.5em !important;
+    height: 2.5em;
+}
+"""
+
+
+examples = [
+    # 1-ToonYou
+    [
+        "toonyou_beta3.safetensors", 
+        "mm_sd_v14.ckpt", 
+        "masterpiece, best quality, 1girl, solo, cherry blossoms, hanami, pink flower, white flower, spring season, wisteria, petals, flower, plum blossoms, outdoors, falling petals, white hair, black eyes",
+        "worst quality, low quality, nsfw, logo",
+        512, 512, "13204175718326964000"
+    ],
+    # 2-Lyriel
+    [
+        "lyriel_v16.safetensors", 
+        "mm_sd_v15.ckpt", 
+        "A forbidden castle high up in the mountains, pixel art, intricate details2, hdr, intricate details, hyperdetailed5, natural skin texture, hyperrealism, soft light, sharp, game art, key visual, surreal",
+        "3d, cartoon, anime, sketches, worst quality, low quality, normal quality, lowres, normal quality, monochrome, grayscale, skin spots, acnes, skin blemishes, bad anatomy, girl, loli, young, large breasts, red eyes, muscular",
+        512, 512, "6681501646976930000"
+    ],
+    # 3-RCNZ
+    [
+        "rcnzCartoon3d_v10.safetensors", 
+        "mm_sd_v14.ckpt", 
+        "Jane Eyre with headphones, natural skin texture,4mm,k textures, soft cinematic light, adobe lightroom, photolab, hdr, intricate, elegant, highly detailed, sharp focus, cinematic look, soothing tones, insane details, intricate details, hyperdetailed, low contrast, soft cinematic light, dim colors, exposure blend, hdr, faded",
+        "deformed, distorted, disfigured, poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, mutated hands and fingers, disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation",
+        512, 512, "2416282124261060"
+    ],
+    # 4-MajicMix
+    [
+        "majicmixRealistic_v5Preview.safetensors", 
+        "mm_sd_v14.ckpt", 
+        "1girl, offshoulder, light smile, shiny skin best quality, masterpiece, photorealistic",
+        "bad hand, worst quality, low quality, normal quality, lowres, bad anatomy, bad hands, watermark, moles",
+        512, 512, "7132772652786303"
+    ],
+    # 5-RealisticVision
+    [
+        "realisticVisionV20_v20.safetensors", 
+        "mm_sd_v15.ckpt", 
+        "photo of coastline, rocks, storm weather, wind, waves, lightning, 8k uhd, dslr, soft lighting, high quality, film grain, Fujifilm XT3",
+        "blur, haze, deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime, mutated hands and fingers, deformed, distorted, disfigured, poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, disconnected limbs, mutation, mutated, ugly, disgusting, amputation",
+        512, 512, "1490157606650685400"
+    ]
+]
+
+
+def ui():
+    with gr.Blocks(css=css) as demo:
+        gr.Markdown(
+            """
+            # AnimateDiff: Animate Your Personalized Text-to-Image Diffusion Models without Specific Tuning
+            Yuwei Guo, Ceyuan Yang*, Anyi Rao, Yaohui Wang, Yu Qiao, Dahua Lin, Bo Dai (*Corresponding Author)<br>
+            [Arxiv Report](https://arxiv.org/abs/2307.04725) | [Project Page](https://animatediff.github.io/) | [Github](https://github.com/guoyww/animatediff/)
+            """
+        )
+        gr.Markdown(
+            """
+            ### Quick Start
+            1. Select desired `Base DreamBooth Model`.
+            2. Select `Motion Module` from `mm_sd_v14.ckpt` and `mm_sd_v15.ckpt`. We recommend trying both of them for the best results.
+            3. Provide `Prompt` and `Negative Prompt` for each model. You are encouraged to refer to each model's webpage on CivitAI to learn how to write prompts for them. Below are the DreamBooth models in this demo. Click to visit their homepage.
+                - [`toonyou_beta3.safetensors`](https://civitai.com/models/30240?modelVersionId=78775)
+                - [`lyriel_v16.safetensors`](https://civitai.com/models/22922/lyriel)
+                - [`rcnzCartoon3d_v10.safetensors`](https://civitai.com/models/66347?modelVersionId=71009)
+                - [`majicmixRealistic_v5Preview.safetensors`](https://civitai.com/models/43331?modelVersionId=79068)
+                - [`realisticVisionV20_v20.safetensors`](https://civitai.com/models/4201?modelVersionId=29460)
+            4. Click `Generate`, wait for ~1 min, and enjoy.
+            """
+        )
+        with gr.Row():
+            with gr.Column():
+                base_model_dropdown     = gr.Dropdown( label="Base DreamBooth Model", choices=c.base_model_list,    value=c.base_model_list[0],    interactive=True )
+                motion_module_dropdown  = gr.Dropdown( label="Motion Module",  choices=c.motion_module_list, value=c.motion_module_list[0], interactive=True )
+
+                base_model_dropdown.change(fn=c.update_base_model,       inputs=[base_model_dropdown],    outputs=[base_model_dropdown])
+                motion_module_dropdown.change(fn=c.update_motion_module, inputs=[motion_module_dropdown], outputs=[motion_module_dropdown])
+
+                prompt_textbox          = gr.Textbox( label="Prompt",          lines=3 )
+                negative_prompt_textbox = gr.Textbox( label="Negative Prompt", lines=3, value="worst quality, low quality, nsfw, logo")
+
+                with gr.Accordion("Advance", open=False):
+                    with gr.Row():
+                        width_slider  = gr.Slider(  label="Width",  value=512, minimum=256, maximum=1024, step=64 )
+                        height_slider = gr.Slider(  label="Height", value=512, minimum=256, maximum=1024, step=64 )
+                    with gr.Row():
+                        seed_textbox = gr.Textbox( label="Seed",  value=-1)
+                        seed_button  = gr.Button(value="\U0001F3B2", elem_classes="toolbutton")
+                        seed_button.click(fn=lambda: gr.Textbox.update(value=random.randint(1, 1e16)), inputs=[], outputs=[seed_textbox])
+
+                generate_button = gr.Button( value="Generate", variant='primary' )
+
+            with gr.Column():
+                result_video = gr.Video( label="Generated Animation", interactive=False )
+                json_config  = gr.Json( label="Config", value=None )
+
+            inputs  = [base_model_dropdown, motion_module_dropdown, prompt_textbox, negative_prompt_textbox, width_slider, height_slider, seed_textbox]
+            outputs = [result_video, json_config]
+            
+            generate_button.click( fn=c.run_once, inputs=inputs, outputs=outputs )
+                
+        gr.Examples( fn=c.run_once(), examples=examples, inputs=inputs, outputs=outputs, cache_examples=True )
+        
+    return demo
+
+
 if __name__ == "__main__":
-    main()
-    c = MagicMeController()
-    c.run_once(None,None)
+    demo = ui()
+    demo.queue(max_size=20)
+    demo.launch(server_port=8860)
